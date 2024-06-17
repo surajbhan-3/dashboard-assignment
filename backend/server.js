c63c0d2415a337574d3bfea12d138a454e00a964
Suraj Bhan Singh <surajbhan2boaz@gmail.com>
2024-06-17 14:36:10 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,24 @@ const corsOptions = {
   optionsSuccessStatus: 204,                               // HTTP status code for successful preflight requests
 };
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://dashboard-assignment-xi.vercel.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 
 app.use(express.json())

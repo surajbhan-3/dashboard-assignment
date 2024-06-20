f19eea580cf719fd16ba61ff4353b02d2a17d205
Suraj Bhan Singh <surajbhan2boaz@gmail.com>
2024-06-20 07:08:20 +0530

    update

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useEffect } from "react";
 import { createContext, useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext()
 
 
--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,6 +1,6 @@
 // // development server
-const API_BASE_URL = 'http://localhost:4500';
-export const AUTH_BASE_URL ='http://localhost:4500';
+// const API_BASE_URL = 'http://localhost:4500';
+// export const AUTH_BASE_URL ='http://localhost:4500';
 
 // production server * vercel
 
@@ -14,7 +14,9 @@ export const AUTH_BASE_URL ='http://localhost:4500';
 // export const AUTH_BASE_URL = 'https://dashboard-assignment-t2ro.onrender.com';
 
 
-
+//* aws
+const API_BASE_URL = 'http://13.235.15.76:4500';
+export const AUTH_BASE_URL = 'http://13.235.15.76:4500';
 
 export const getToken = () => localStorage.getItem("token");
 

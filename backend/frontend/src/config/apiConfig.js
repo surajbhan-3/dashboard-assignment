80ccdae23eb1539bf4cbaa6e972256c198022ea2
Suraj Bhan Singh <surajbhan2boaz@gmail.com>
2024-06-22 15:10:47 +0530

    vercel-deployment

---
+++ backend/frontend/src/config/apiConfig.js
@@ -0,0 +1,49 @@
+// // development server
+// const API_BASE_URL = 'http://localhost:4500';
+// export const AUTH_BASE_URL ='http://localhost:4500';
+
+
+// production server * vercel
+
+// const API_BASE_URL = 'https://dashboard-assignment-ec41.vercel.app/';
+// export const AUTH_BASE_URL = 'https://dashboard-assignment-ec41.vercel.app/';
+
+// * render
+// const API_BASE_URL = 'https://dashboard-assignment-t2ro.onrender.com';
+// export const AUTH_BASE_URL = 'https://dashboard-assignment-t2ro.onrender.com';
+
+
+//* aws
+// const API_BASE_URL = 'http://13.235.15.76:4500';
+// export const AUTH_BASE_URL = 'http://13.235.15.76:4500';
+
+//* aws elastic beanstalk
+// const API_BASE_URL = 'http://kryzen-app-env.eba-mhj6baup.ap-south-1.elasticbeanstalk.com';
+// export const AUTH_BASE_URL = 'http://kryzen-app-env.eba-mhj6baup.ap-south-1.elasticbeanstalk.com';
+
+
+//* aws elastic loadblancer
+// const API_BASE_URL = 'https://krylb-2021498952.ap-south-1.elb.amazonaws.com';
+// export const AUTH_BASE_URL = 'https://krylb-2021498952.ap-south-1.elb.amazonaws.com';
+
+//* verdel
+
+const API_BASE_URL = `${window.location.origin}`;
+export const AUTH_BASE_URL = `${window.location.origin}`;
+
+export const getToken = () => localStorage.getItem("token");
+
+
+const apiConfig = {
+  baseURL: `${API_BASE_URL}/api/product`,
+
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${getToken()}`,
+  },
+  timeout: 5000, // Request timeout in milliseconds
+};
+
+
+
+export default apiConfig;
--- backend/index.js
+++ backend/index.js
@@ -4,42 +4,31 @@ require("dotenv").config()
 const {prisma} = require("./config/db")
 const userRouter = require("./routes/userRoutes")
 const productRouter = require("./routes/productRoutes")
-
+const path = require("path")
 const PORT = process.env.PORT 
-const corsOptions = {
-  // origin: 'https://dashboard-assignment-xi.vercel.app',
-  origin: ['https://dashboard-assignment-xi.vercel.app','http://localhost:3000'],
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+
 
 
 const app = express()
 
 app.use(express.json())
-app.use(cors(corsOptions))
 
 
 
 
 
 
-app.get("/", async(req,res)=>{
-
-    try {
-        res.status(200).send("Welcome to the Backend of Lms")
-    } catch (error) {
-        res.status(500).send(
-            {
-                "message":"Internal Server Errer",
-                "Error":error.message,
-                "result":false
-            }
-        )
-    }
-})
 
 app.use("/api/user", userRouter)
 app.use("/api/product", productRouter)
+
+
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
+
+
 
 //
 app.use((err, req, res, next) => {
